3c54a4c43a1d0d3348b8108102255aa0d856da77
Tej1204 <ptkshiteez@gmail.com>
2020-07-23 17:11:06 +0400

    Created a new program to add up the its digits.

---
+++ Sum_of_Digits.py
@@ -0,0 +1,10 @@
+num = input("Enter any number: ")
+sum = 0
+
+a = 945 % 10
+b = 945//10
+c = b % 10
+d = b//10
+e = d % 10
+
+print(a,c,e)
--- Swap_Variables.py
+++ Swap_Variables.py
@@ -1,1 +1,18 @@
-# Test
+a = 14
+b = 20
+
+# Method 1:
+# c = a
+# a = b
+# b = c
+
+# Method 2
+# b = b+a
+# a = b-a
+# b = b-a
+
+# Method 3:
+# a, b = b, a
+
+print(a)
+print(b)
