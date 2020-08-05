1e4405936c7919552119e23960f3ec2f2a20a790
Tej1204 <ptkshiteez@gmail.com>
2020-08-05 11:34:55 +0400

    Created a new program to change 0 to 5 in a number.

---
+++ 0_to_5.py
@@ -0,0 +1,14 @@
+def convert_to_five(value):
+    num2 = 0
+    for i in range(len(str(value))):
+        dig = value % 10
+        if dig == 0:
+            dig = dig + 5
+        value = value // 10
+        dig = dig * 10**(i)
+        num2 = num2 + dig
+    print(num2)
+
+
+value = int(input("Please enter a number: "))
+convert_to_five(value)
